5be3ffc50162751316a77efd5f25c008d5b961fc
Vikaskanwate <vikaskanwate32@gmail.com>
2024-12-26 19:27:26 +0530

    css

--- js/script.js
+++ js/script.js
@@ -122,26 +122,34 @@ function swiper(){
     });
 }
 
-var menu =document.querySelector('nav h3');
-var full = document.querySelector('.full-scr');
-var navImg = document.querySelector('nav img');
-var flag = 0;
-menu.addEventListener('click',function(){
-    if(flag == 0){
-        full.style.top = 0;
-        navImg.style.opacity = 0;
-        flag=1;
-        full.style.transition = 'all 0.4s ease';
-    }
-    else 
-    {
-        full.style.top = '-120%';
-        navImg.style.opacity = 1;
-        flag = 0;
-    }
-    
-})
+function menuAni(){
+    var menu =document.querySelector('nav h3');
+    var full = document.querySelector('.full-scr');
+    var navImg = document.querySelector('nav img');
+    var flag = 0;
+    menu.addEventListener('click',function(){
+        if(flag == 0){
+            full.style.top = 0;
+            navImg.style.opacity = 0;
+            flag=1;
+            full.style.transition = 'all 0.4s ease';
+        }
+        else 
+        {
+            full.style.top = '-120%';
+            navImg.style.opacity = 1;
+            flag = 0;
+        }
+        
+    })
+}
+ 
+var loader = document.querySelector('.loader');
 
+setTimeout(() => {
+    loader.style.top = "-100%";
+}, 2000);
+menuAni();
 scroll();
 page2();
 page4();
